f0477ae943854e0c9e80ff61b411ab37761bcb9d
Cansino Oliva <Cansino Oliva@PCBOX>
2021-04-12 17:14:30 -0300

    Proyecto de prueba Emilio Cansino Oliva

---
+++ src/main/java/ar/edu/unju/edm/PrimerProyectoSpringEdmAplication.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerProyectoSpringEdmAplication {
+	public static  void main(String[] args) {
+		SpringApplication.run(PrimerProyectoSpringEdmAplication.class, args);
+		System.out.println("Hola Mundo, soy Emilio");
+	}
+}
